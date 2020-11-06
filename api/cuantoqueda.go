272b632ee7d274b164fb0aae04ca84d8bb4fee4d
JJ Merelo <jjmerelo@gmail.com>
2020-11-06 12:56:30 +0100

    Para hacer experimentos

--- api/cuantoqueda.go
+++ api/cuantoqueda.go
@@ -6,7 +6,40 @@ import (
     "time"
 )
 
+type Hito struct {
+	URI  string
+	Title string
+	Date time.Time
+}
+
+
+var hitos = []Hito {
+	Hito {
+		URI: "0.Repositorio",
+		Title: "Datos básicos y repo",
+		Date: time.Date(2020, time.September, 29, 11, 30, 0, 0, time.UTC),
+	},
+	Hito {
+		URI: "1.Infraestructura",
+		Title: "HUs y entidad principal",
+		Date: time.Date(2020, time.October, 6, 11, 30, 0, 0, time.UTC),
+	},
+	Hito {
+		URI: "2.Tests",
+		Title: "Tests iniciales",
+		Date: time.Date(2020, time.October, 16, 11, 30, 0, 0, time.UTC),
+	},
+}
+
+
 func Handler(w http.ResponseWriter, r *http.Request) {
-    currentTime := time.Now().Format(time.RFC850)
-    fmt.Fprintf(w, currentTime)
+	var currentTime Time := time.Now()
+	fmt.Println( currentTime )
+	var next
+	for indice, hito := range hitos {
+		if ( hito.Date.after( currentTime ) ) {
+			next = indice
+		}
+	}
+	fmt.Fprintf(w, currentTime)
 }
---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"time"
+	"fmt"
+)
+
+type Hito struct {
+	URI  string
+	Title string
+	fecha time.Time
+}
+
+
+var hitos = []Hito {
+	Hito {
+		URI: "0.Repositorio",
+		Title: "Datos básicos y repo",
+		fecha: time.Date(2020, time.September, 29, 11, 30, 0, 0, time.UTC),
+	},
+	Hito {
+		URI: "1.Infraestructura",
+		Title: "HUs y entidad principal",
+		fecha: time.Date(2020, time.October, 6, 11, 30, 0, 0, time.UTC),
+	},
+	Hito {
+		URI: "2.Tests",
+		Title: "Tests iniciales",
+		fecha: time.Date(2020, time.October, 16, 11, 30, 0, 0, time.UTC),
+	},
+	Hito {
+		URI: "3.Contenedores",
+		Title: "Contenedores",
+		fecha: time.Date(2020, time.October, 26, 11, 30, 0, 0, time.UTC),
+	},
+	Hito {
+		URI: "4.CI",
+		Title: "Integración continua",
+		fecha: time.Date(2020, time.November, 6, 23, 59, 0, 0, time.UTC),
+	},
+
+}
+
+func main() {
+	currentTime := time.Now()
+	var next int
+	var queda time.Duration
+	for indice, hito := range hitos {
+		if ( hito.fecha.After( currentTime ) ) {
+			next = indice
+			queda = hito.fecha.Sub( currentTime )
+		}
+	}
+	if ( next > 0) {
+		fmt.Println( next, queda, hitos[next] )
+	}
+}
