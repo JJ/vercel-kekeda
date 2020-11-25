21f5ac4f3efa6a5d9d741f5b6941bd8667744941
Yabir Garcia <yabirg@protonmail.com>
2020-11-25 10:49:25 +0100

    Añade los dias que quedan cuando la cantidad de horas es mayor que 24

--- api/kekeda_iv.go
+++ api/kekeda_iv.go
@@ -22,6 +22,33 @@ type Response struct {
 	Method string `json:"method"`
 }
 
+func format_date(diff time.Duration) string {
+	// Función para convertir una diferencia de fechas
+	// a una cadena del tipo dias horas minutos segundos
+
+    // Definimos constantes para partir la diferencia
+    const Decisecond = 100 * time.Millisecond
+    const Day = 24*time.Hour
+
+    // Extraemos la cantidad de dias y las quitamos de la diferencia
+    d := diff / Day
+    diff = diff % Day
+    // Extraemos la cantidad de horas
+    h := diff / time.Hour
+    diff = diff % time.Hour
+    // Extraemos la cantidad de minutos
+    m := diff / time.Minute
+    diff = diff % time.Minute
+    // Extraemos la cantidad de secundos
+    s := diff / time.Second
+    diff = diff % time.Second
+    // Nos quedamos con las partes de segundo
+    f := diff / Decisecond
+    return  fmt.Sprintf("%dd %dh %dm %d.%ds", d, h, m, s, f)
+
+}
+
+
 var hitos = []Hito {
 	Hito {
 		URI: "0.Repositorio",
@@ -97,7 +124,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 
 			switch update.Message.Command() {
 			case "kk":
-				text = queda.String()
+				text = format_date(queda)
 			case "kekeda":
 				text = fmt.Sprintf( "→ Próximo hito %s\n🔗 https://jj.github.io/IV/documentos/proyecto/%s\n📅 %s",
 					hitos[next].Title,
